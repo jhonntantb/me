819f0af1f076a278abbedebaf8ded5236bb83064
jhonntantb <jhonntantb@gmail.com>
2022-04-04 22:29:40 -0500

    fix form

--- form-to-email.php
+++ form-to-email.php
@@ -4,20 +4,20 @@
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-  $email_from = 'jhonntantb@yourwebsite.com';
+  $email_from = 'alejhotabraj@gmail.com';
 
 	$email_subject = "Email to contact me";
 
-	$email_body = "You have received a new message from the user $name.\n".
-                "Whit the Email is: $visitor_email.\n".
-                "the subject is: $subject.\n".
-                "Here is the message:\n $message".;
+	$email_body = "You have received a new message from the user $name" . "\r\n";
+  $email_body .= "Whit the Email is: $visitor_email" . "\r\n";
+  $email_body .= "the subject is: $subject" . "\r\n";
+  $email_body .=  "Here is the message:\n $message";
   
   $to = "jhonntantb@gmail.com";
 
-  $headers = "From: $email_from \r\n";
-              
+  $headers = "From: $email_from \r\n";   
   $headers .= "Reply-To: $visitor_email \r\n";
+  $headers .= "X-Mailer: PHP/" . phpversion();
               
   mail($to,$email_subject,$email_body,$headers);
 ?>
